fe6b6e132b4fba0fe3ec9bf4d06f4c749a9ef192
patrickmcd-07 <patrickmcd2007@gmail.com>
2025-11-04 08:32:32 +0100

    Update coffeeMachine.basic.test.ts

--- tests/coffeeMachine.basic.test.ts
+++ tests/coffeeMachine.basic.test.ts
@@ -1,17 +1,40 @@
+import { CoffeeMachine } from "../src/coffeeMachine";
+import { Drink } from "../src/drink";
+
 describe('CoffeeMachine basic', () => {
   it('serves small coffee with exact money', () => {
+    const machine = new CoffeeMachine();
 
+    const drink = new Drink("Coffee", 2, false, 0, "small");
+
+    expect(machine.serve(drink, 2, false, 10)).toBe("Serving Coffee (small)");
   });
 
   it('serves tea and returns change', () => {
+    const machine = new CoffeeMachine();
 
+    const drink = new Drink("Tea", 2, false, 0, "small");
+
+    expect(machine.serve(drink, 2.5, false, 10)).toBe("Serving Tea (small) with change 0.50");
   });
 
   it('applies milk surcharge', () => {
-
+    //arrange
+    const machine = new CoffeeMachine();
+    const drink = new Drink("Coffee", 2, true, 0, "small");
+    //act
+    const result =  machine.serve(drink, 2.2, false, 10)
+    //assert
+    expect(result).toBe("Serving Coffee (small)");
   });
 
   it('applies sugar surcharge above 2 cubes', () => {
-
+    //arrange
+    const machine = new CoffeeMachine();
+    const drink = new Drink("Coffee", 2, false, 3, "small");
+    //act
+    const result =  machine.serve(drink, 2.1, false, 10)
+    //assert
+    expect(result).toBe("Serving Coffee (small)");
   });
 });
