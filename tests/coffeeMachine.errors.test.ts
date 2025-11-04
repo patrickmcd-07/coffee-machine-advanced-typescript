cb17c8471a064f9e26cb94c0eb7104fc895a68a0
patrickmcd-07 <patrickmcd2007@gmail.com>
2025-11-04 08:33:08 +0100

    Update coffeeMachine.errors.test.ts

--- tests/coffeeMachine.errors.test.ts
+++ tests/coffeeMachine.errors.test.ts
@@ -1,3 +1,23 @@
-describe('CoffeeMachine errors', () => {
+import { CoffeeMachine } from "../src/coffeeMachine";
+import { Drink } from "../src/drink";
 
+describe('CoffeeMachine errors', () => {
+      it('throws too much sugar error if sugar is more than 5', () => {
+        const machine = new CoffeeMachine();
+        const drink = new Drink("Latte", 3.5, true, 6, "medium");
+        const wrapper = () => machine.serve(drink, 2, false, 1);
+        expect(wrapper).toThrow("Too much sugar");
+      });
+      it('throws Invalid price error if price is less than 0',()=>{
+        const machine = new CoffeeMachine();
+        const drink = new Drink("Latte", -5, true, 2, "medium");
+        const wrapper = () => machine.serve(drink, 2, false, 1);
+        expect(wrapper).toThrow("Invalid price");
+      })
+    it('throws Not enough money error if inserted money is less than price',()=>{
+        const machine = new CoffeeMachine();
+        const drink = new Drink("Latte", 3.5, true, 2, "medium");
+        const wrapper = () => machine.serve(drink, 3, false, 1);
+        expect(wrapper).toThrow("Not enough money");
+      })
 });
