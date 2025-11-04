6609f41dc3b12337cec9d65d817bea26b4a70e2d
patrickmcd-07 <patrickmcd2007@gmail.com>
2025-11-04 08:33:27 +0100

    Update coffeeMachine.mocks.test.ts

--- tests/coffeeMachine.mocks.test.ts
+++ tests/coffeeMachine.mocks.test.ts
@@ -1,3 +1,20 @@
-describe('CoffeeMachine notifications (mocks)', () => {
+import { CoffeeMachine } from "../src/coffeeMachine";
+import { Drink } from "../src/drink";
 
+import * as notification from '../src/utils/notifications';
+
+describe('CoffeeMachine notifications (mocks)', () => {
+    beforeEach(()=>{
+        jest.resetAllMocks
+    })
+    it('notifies user about serving drink', () => {
+        const machine = new CoffeeMachine();
+        const drink = new Drink("Coffee", 2, false, 0, "small");
+        const notifyUserSpy = jest.spyOn(notification,"notifyUser").mockImplementation(jest.fn());
+        machine.serve(drink, 2, false, 10)
+
+        expect(notifyUserSpy).toHaveBeenCalled()
+        // expect(notifyUserSpy).toHaveBeenCalledWith('message')
+        expect(notifyUserSpy).toHaveBeenCalledTimes(1)
+    })
 });
